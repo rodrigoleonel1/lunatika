71cf8620d732ab26b2b2503ed840ae55edbda1aa
Rodrigo Alarcón <rodricsxd@gmail.com>
2024-12-15 13:30:29 -0300

    delete req from endpoints that don't use it

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,7 +1,7 @@
 import { supabase } from "@/lib/supabase";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET() {
   try {
     const { data, error } = await supabase.from("category").select("*");
 
