6fae8831cd2470a594956cda91748f1a3be5d12b
Rodrigo Alarcón <rodricsxd@gmail.com>
2024-12-27 16:59:30 -0300

    update category cards

--- components/categories-list.tsx
+++ components/categories-list.tsx
@@ -16,12 +16,12 @@ export const CategoriesList = ({ title, items }: CategoriesListProps) => {
       <Heading title={title} description="Explora nuestras categorías." />
 
       {items.length === 0 && <NoResults />}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4 mt-4">
         {items.map((item) => (
           <Link
             href={`/category/${item.name}`}
             key={item.id}
-            className="flex flex-col items-center justify-between bg-white rounded-lg gap-4 shadow-md p-4"
+            className="flex flex-col items-center justify-between bg-white rounded-lg gap-4 shadow-md p-2 md:p-4"
           >
             <div className="w-full overflow-hidden rounded-lg bg-gray-200">
               <img
@@ -31,7 +31,7 @@ export const CategoriesList = ({ title, items }: CategoriesListProps) => {
               />
             </div>
             <div className="flex justify-between w-full items-center">
-              <p className="text-xl font-semibold">{item.name}</p>
+              <p className="text-lg font-medium md:text-xl">{item.name}</p>
               <IoChevronForward size={20} className="mt-1" />
             </div>
           </Link>
