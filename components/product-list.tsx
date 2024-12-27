1034307017d95ca96b56c6bd3536ced314174856
Rodrigo Alarcón <rodricsxd@gmail.com>
2024-12-26 23:21:37 -0300

    update page funcionality

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -4,8 +4,6 @@ import { Heading } from "./ui/heading";
 import NoResults from "./ui/no-results";
 import { Container } from "./container";
 
-import { IoChevronForward } from "react-icons/io5";
-
 interface ProductListProps {
   title: string;
   items: Product[];
@@ -16,8 +14,6 @@ export default function ProductList({ title, items }: ProductListProps) {
     <Container>
       <div className="flex items-center justify-between mb-4">
         <Heading title={title} description="" />
-        <p>Ver todos los destacados</p>
-        <IoChevronForward />
       </div>
       {items.length === 0 && <NoResults />}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
